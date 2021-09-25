d0b633ba2369c28b5adc0f5c5ca662a6d951a7e8
srirubas <rubas9821@gmail.com>
2021-09-25 16:19:15 +0530

    ruba added this code

--- src/org/git/NewGit.java
+++ src/org/git/NewGit.java
@@ -1,8 +1,12 @@
 package org.git;
 
 public class NewGit {
+	private void m1() {
+System.out.println("user name ");
+	}
 	public static void main(String[] args) {
 		System.out.println("sriruba");
+		
 	}
 
 }
